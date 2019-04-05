a8cabf9f1dfb1646e2862afcd1c37a08eaaa87df
Ahmad Ammar Asyraaf Jainuddin <aaasyraafj@gmail.com>
2019-04-05 15:38:37 +0800

    Added schematics

---
+++ v1/v0.7/robot/main.py
@@ -0,0 +1,63 @@
+"""
+Robot-Dev Code Version 0.7.0-01042019
+This code is design to be used with Raspberry Pi Zero W
+
+*This code is to function as a slave following one master (server)
+instructions.
+*print function is used for debugging purposes.
+*log.write_log are use to keep record of the program
+
+"""
+# Direct import
+import cv2
+import pickle
+
+# Partial import
+from easylog import EasyLog
+#from dcmcontrol import DCMControl
+from tcphandler import TCPHandler
+
+# Global variables
+name = "Robot 1"
+log = EasyLog() # Initialize EasyLog
+
+# Camera
+cap = cv2.VideoCapture(0)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+def cmd_exec(cmd):
+    if cmd == "frame":
+        ret, frame = cap.read()
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pass
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(frame, 0)
+        return data
+    return ""
+
+def process_data(data):
+    print(data.key)
+    if data.key == 1:
+        data.key = 2
+        data.data = "Connection established!"
+        return data
+    elif data.key == 1000:
+        data.data = data.data.decode("utf-8")
+        print(data.data)
+        data.data = cmd_exec(data.data)
+        data.key = 1001
+        return data
+    return data
+
+if __name__ == "__main__":
+    conn = TCPHandler()
+    while True:
+        ret = conn.check_conn()
+        if not ret:
+            buffer = conn.get_buffer_data()
+            if buffer:
+                for each in buffer:
+                    data = each[0]
+                    data = process_data(each[0])
+                    conn.ready_data(buffer.index(each), data.key, data.data)
---
+++ v1/v0.7/server/main.py
@@ -0,0 +1,64 @@
+"""
+Robot-Dev Code Version 0.7.0-01042019
+This code is design to be used with a windows computer
+
+*This code is to function as a master giving instruction to many slaves.
+*print function is used for debugging purposes.
+
+"""
+import cv2
+import pickle
+
+from easylog import EasyLog
+from tcphandler import TCPHandler
+
+class RobotMaster():
+    pass
+
+def proc_reply(data):
+    print(data)
+    try:
+        frame=pickle.loads(data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        cv2.imshow('ImageWindow',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pass
+    except:
+        pass
+
+def process_data(data):
+    print(data.key)
+    if data.key == 1:
+        data.key = 2
+        data.data = "Connection established!"
+        return data
+    elif data.key == 2:
+        data.key = 1000
+        data.data = "frame"
+        return data
+    elif data.key == 1000:
+        data.data = data.data.decode("utf-8")
+        #data.data = cmd_exec(data.data)
+        data.key = 1001
+        return data
+    elif data.key == 1001:
+#         if data.data.decode("utf-8") == "frame":
+#             return data
+        proc_reply(data.data)
+        data.key = 1000
+        data.data = "frame"
+        return data
+    return data
+
+if __name__ == "__main__":
+    conn = TCPHandler(port=5001)
+    conn.connect("192.168.1.17", 5000, 1, "Handshake")
+    while True:
+        ret = conn.check_conn()
+        if not ret:
+            buffer = conn.get_buffer_data()
+            if buffer:
+                for each in buffer:
+                    data = each[0]
+                    data = process_data(each[0])
+                    conn.ready_data(buffer.index(each), data.key, data.data)
